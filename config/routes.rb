40f877876b36ed35c31c9130572c1f7cb04b2204
g-mac <simonmayrshofer@hotmail.com>
2016-05-24 12:57:12 +0200

    add articles resource

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,13 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
+  resources :articles
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  #   get 'products/:id/purchase' => 'catalog#purchase',add as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
