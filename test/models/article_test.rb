d1e4569a066ace88a318cb669eeca7fc96782ab9
g-mac <simonmayrshofer@hotmail.com>
2016-06-03 00:22:59 +0200

    wrote tests for article and comment model (test validations)

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should not save article without title" do
+    article = Article.new
+    article.text = "This is an example text."
+    assert_not article.save
+  end
+
+  test "should not save article wiht title shorter than 5 chars" do
+    article = Article.new
+    article.title = "T"
+    article.text = "This is an example text."
+    assert_not article.save
+  end
+
+  test "should not save article without text" do
+    article = Article.new
+    article.title = "Title"
+    assert_not article.save
+  end
+
+  test "should not save article wiht text shorter than 10 chars" do
+    article = Article.new
+    article.title = "Title"
+    article.text = "ShortText"
+    assert_not article.save
+  end
+
 end
--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should not create comment without commenter" do
+    # comment = Comment.create(body: "Test body.") # another way option
+    comment = Comment.new
+    comment.body = "Test body."
+    assert_not comment.save
+  end
+
+  test "should not create comment without body" do
+    comment = Comment.new
+    comment.commenter = "Testcommenter"
+    assert_not comment.save
+  end
+
 end
