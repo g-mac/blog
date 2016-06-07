9a2e328eea09b738de6152e93a583c2294cdce69
g-mac <simonmayrshofer@hotmail.com>
2016-06-07 14:28:21 +0200

    implemented login/signup. models: user; controllers: users, sessions; and layout changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,26 @@
 class SessionsController < ApplicationController
+
   def new
   end
+
+  def create
+    user = User.authenticate(params[:email], params[:password])
+    if user
+      session[:user_id] = user.id
+      if URI(request.referrer).path == log_in_path || URI(request.referrer).path == sign_up_path
+        redirect_to root_url #, :notice => 'Logged out!'
+      else
+        redirect_to request.referrer #, :notice => 'Logged in!'
+      end
+    else
+      # flash.now.alert = 'Invalid email or password'
+      redirect_to log_in_path, :notice => 'Invalid email or password!'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url #, :notice => 'Logged out!'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  get 'users/new'
-
+  # get 'sessions/new'
+  # get 'users/new'
+  # instead use:
+  get 'log_out' => 'sessions#destroy', :as => 'log_out'
+  get 'log_in' => 'sessions#new', :as => 'log_in'
+  get 'sign_up' => 'users#new', :as => 'sign_up'
   get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -11,6 +13,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
+  resources :users
+  resources :sessions
   resources :articles do
     resources :comments
   end
