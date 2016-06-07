c3fc0e048bf61358995e0d8053d0793bfc947568
g-mac <simonmayrshofer@hotmail.com>
2016-06-08 01:25:48 +0200

    created new page (portfolio), added the route and layout and made some changes to navbar

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'log_in' => 'sessions#new', :as => 'log_in'
   get 'sign_up' => 'users#new', :as => 'sign_up'
   get 'welcome/index'
+  get 'welcome/portfolio'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
