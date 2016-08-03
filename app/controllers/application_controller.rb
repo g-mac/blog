bab08f774254703f9320a34df237d6a272bd824b
g-mac <simonmayrshofer@hotmail.com>
2016-08-03 20:33:08 +0200

    implement use of Simple Token Authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  # Simple Token Authentication handling: https://github.com/gonzalo-bulnes/simple_token_authentication
+  # use for api? or whole app?
+  # acts_as_token_authentication_handler_for User
+
 end
