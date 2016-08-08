e4f93100576a4b0fce72703e47c2d177ef9465b8
g-mac <simonmayrshofer@hotmail.com>
2016-08-08 17:27:56 +0200

    implement json response for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,6 +5,10 @@ class ArticlesController < ApplicationController
 
   def index
     @articles = Article.all
+    respond_to do |format|
+      format.html
+      format.json { render json: @articles }
+    end
   end
 
   def show
