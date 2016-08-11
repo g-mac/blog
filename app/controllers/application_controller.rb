1ee3379251124b75c3892d4457339fd9968929e5
g-mac <simonmayrshofer@hotmail.com>
2016-08-11 03:35:01 +0200

    attempt to allow json logins (AJAX)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  skip_before_action :verify_authenticity_token
 
   # Simple Token Authentication handling: https://github.com/gonzalo-bulnes/simple_token_authentication
   # use for api? or whole app?
