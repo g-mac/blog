bbe1264b8b3abc934c94b1f54913de40551c10a8
g-mac <simonmayrshofer@hotmail.com>
2016-08-11 03:17:16 +0200

    attempt to allow json logins (AJAX)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { :omniauth_callbacks => 'users/omniauth_callbacks' }
+
+  devise_for :users, :controllers => { :omniauth_callbacks => 'users/omniauth_callbacks' }, :controllers => {sessions: 'sessions'}
   # get 'sessions/new'
   # get 'users/new'
   # instead use:
