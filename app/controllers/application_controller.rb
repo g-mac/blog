14c27f3e62125cca19339a769b234e6688337864
g-mac <simonmayrshofer@hotmail.com>
2016-08-23 13:16:19 +0200

    RABL: remove outer default HTML from rabl json responses

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,18 @@ class ApplicationController < ActionController::Base
   # use for api? or whole app?
   # acts_as_token_authentication_handler_for User
 
+
+  # this is to make sure that the default application.slim layout is not used for json responses
+
+  layout :mime_layout
+
+  private def mime_layout
+    case params[:format]
+      when 'json'
+        false
+      else
+        'application'
+    end
+  end
+
 end
