d49a652ae823ca754f5f1d599ecdae9d066c3797
g-mac <simonmayrshofer@hotmail.com>
2016-08-23 13:23:26 +0200

    added comment on necessary security fix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,15 @@
 class SessionsController < Devise::SessionsController
+  #need to fix this security hole
   skip_before_action :verify_authenticity_token
+  # skip_before_action :verify_authenticity_token, :only => [:new] #need to fix this security hole
+
   respond_to :json
+
+  # def new
+  #   respond_to do |format|
+  #     format.html { super }
+  #     format.json { render json: current_user }
+  #   end
+  # end
+
 end
