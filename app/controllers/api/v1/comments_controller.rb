d7e54530d20788750dffd53b7f564804693dbdb1
g-mac <simonmayrshofer@hotmail.com>
2016-08-30 17:46:28 +0200

    implement article json response for comments api create action

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -13,15 +13,14 @@ module Api
       def create
         @article = Article.find(params[:article_id])
         @comment = @article.comments.create(comment_params)
-        head :ok
-        # redirect_to article_path(@article)
+        # head :ok
+        render :json => @article
       end
 
       def destroy
         @article = Article.find(params[:article_id])
         @comment = @article.comments.find(params[:id])
         @comment.destroy
-        # redirect_to article_path(@article)
       end
 
       private
